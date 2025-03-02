c1dde932fd9da2e2e97a1ef93f8c110fad1fc734
Pranav Rajput <92659078+Pranav970@users.noreply.github.com>
2025-03-02 18:49:33 +0530

    Update mongo_setup.py

--- projects/ Analyzing Global News Sentiment and its Correlation with Cryptocurrency Price Fluctuations/src/database/mongo_setup.py
+++ projects/ Analyzing Global News Sentiment and its Correlation with Cryptocurrency Price Fluctuations/src/database/mongo_setup.py
@@ -1,1 +1,26 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("MONGO_DB_NAME")
+
+def connect_mongodb():
+    """
+    Connects to MongoDB and returns the database object.
+    """
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client[DB_NAME]
+        print("Connected to MongoDB successfully!")
+        return db
+    except Exception as e:
+        print(f"Error connecting to MongoDB: {e}")
+        return None
+
+if __name__ == '__main__':
+    db = connect_mongodb()
+    if db:
+        print(f"Connected to database: {db.name}")
